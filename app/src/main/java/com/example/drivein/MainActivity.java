4719028ea814fcbd016edd5d0b638cdc24b148c1
ShashiKalum11 <COHNDSE241F-120@student.nibm.lk>
2025-05-22 11:07:43 +0530

    Loading

--- app/src/main/java/com/example/drivein/MainActivity.java
+++ app/src/main/java/com/example/drivein/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.drivein;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button redirectButton = findViewById(R.id.btnNext);
+
+
+        redirectButton.setOnClickListener(v -> {
+
+            Intent intent = new Intent(MainActivity.this, Loading_activity.class);
+            startActivity(intent);
+
+            finish();
+        });
 
     }
 }
