58aa8b95a6e0260265945df6154791534e86ca8f
Denula2003 <dadwickramanayaka@gmail.com>
2025-05-26 21:27:38 +0530

    den

---
+++ app/src/main/java/com/example/drivein/My_booking.java
@@ -0,0 +1,13 @@
+package com.example.drivein;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class My_booking extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_booking);
+    }
+}
